db5f4cb6208cdfb66cec082ff9d66f26e77aa5bb
Mike Repec <mikerepec@gmail.com>
2024-11-02 00:15:52 +0100

    feat: add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+import daisyui from 'daisyui';
+
 module.exports = {
+  plugins: [daisyui],
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}
+    './app/javascript/**/*.js',
+  ],
+};
