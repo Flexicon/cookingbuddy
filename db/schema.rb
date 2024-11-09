ef3b76d2624e7292822b273dbe6e95de643955fb
Mike Repec <mikerepec@gmail.com>
2024-11-09 11:34:36 +0100

    feat: upgrade to rails 8.0 :tada:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_03_122608) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_03_122608) do
   create_table "ingredients", force: :cascade do |t|
     t.integer "amount", null: false
     t.string "unit"
