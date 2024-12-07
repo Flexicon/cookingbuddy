443be4912f068f9e1fa9b4afa86889d3e22c34cd
Mike Repec <mikerepec@gmail.com>
2024-12-07 15:41:53 +0100

    chore: watch all component files for tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,11 @@ import daisyui from 'daisyui';
 module.exports = {
   plugins: [daisyui],
   content: [
-    './app/components/**/*.html.erb',
+    './app/components/**/*',
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js',
   ],
-  safelist: [
-    'alert-alert',
-    'alert-error',
-    'alert-success',
-    'alert-info',
-    'btn-alert',
-    'btn-error',
-    'btn-success',
-    'btn-info',
-  ]
+  safelist: []
 };
