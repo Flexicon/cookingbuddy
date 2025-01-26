3941b5144b50ac783ab3de922a1b8f90f1c0b661
Mike Repec <mikerepec@gmail.com>
2025-01-26 12:57:03 +0100

    test: form errors component conditional rendering

---
+++ test/components/form_errors_component_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class FormErrorsComponentTest < ViewComponent::TestCase
+  def setup
+    @model = Recipe.new
+    @model.valid?
+
+    @valid_model = recipes(:one)
+    @valid_model.valid?
+  end
+
+  def test_component_renders
+    render_inline(FormErrorsComponent.new(model: @model))
+
+    assert_text("Oops, couldn't save this Recipe", normalize_ws: true)
+  end
+
+  def test_component_renders_with_valid_model
+    render_inline(FormErrorsComponent.new(model: @valid_model))
+
+    refute_text("Oops, couldn't save this Recipe", normalize_ws: true)
+  end
+end
