8aa0b130329aea0511667c06e87aa7db59fd5e16
Mike Repec <mikerepec@gmail.com>
2025-05-25 16:09:59 +0200

    fix: cleanup modals when closed

--- app/javascript/controllers/modals_controller.js
+++ app/javascript/controllers/modals_controller.js
@@ -1,15 +1,20 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="modals"
 export default class extends Controller {
   connect() {
-    if (typeof this.element.showModal === 'function') {
+    if (typeof this.element.showModal === "function") {
       this.element.showModal();
+
+      // Clean up the modal when it is closed
+      this.element.addEventListener("close", (_e) => {
+        this.element.remove();
+      });
     }
   }
 
   close(e) {
-    if (typeof this.element.close === 'function') {
+    if (typeof this.element.close === "function") {
       e.preventDefault();
       this.element.close();
     }
