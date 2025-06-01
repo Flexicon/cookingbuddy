8d5780ad96786b01394484ca0075b1ab9c459105
Mike Repec <mikerepec@gmail.com>
2025-06-01 12:20:25 +0200

    fix: hide categories on "by category" recipes widget

--- app/components/recipe_list_component.rb
+++ app/components/recipe_list_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class RecipeListComponent < ViewComponent::Base
-  def initialize(recipes:, blank_message: "No recipes found")
+  def initialize(recipes:, blank_message: "No recipes found", show_categories: true)
     @recipes = recipes
     @blank_message = blank_message
+    @show_categories = show_categories
   end
 end
--- app/components/recipe_list_item_component.rb
+++ app/components/recipe_list_item_component.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class RecipeListItemComponent < ViewComponent::Base
-  def initialize(recipe:)
+  def initialize(recipe:, show_category: false)
     @recipe = recipe
+    @show_category = show_category
   end
 end
