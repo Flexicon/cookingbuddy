3fbabc9d3843dd7f4b2bb1e554843ae023634832
Mike Repec <mikerepec@gmail.com>
2025-06-02 19:09:08 +0200

    feat: reuse dropdown component for main settings menu

--- app/components/dropdown_component.rb
+++ app/components/dropdown_component.rb
@@ -5,11 +5,33 @@ class DropdownComponent < ViewComponent::Base
   renders_many :items
 
   DEFAULT_TRIGGER_CLASS = "btn btn-ghost"
-  DEFAULT_MENU_CLASS = "dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+  DEFAULT_MENU_CLASS = "menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
 
-  def initialize(trigger_class: DEFAULT_TRIGGER_CLASS, menu_class: DEFAULT_MENU_CLASS, item_class: nil)
+  POSITIONS = %w[top bottom left right]
+
+  def initialize(
+    container_class: nil,
+    trigger_class: DEFAULT_TRIGGER_CLASS,
+    menu_class: DEFAULT_MENU_CLASS,
+    item_class: nil,
+    hover: false,
+    align_end: false,
+    position: "bottom"
+  )
+    if POSITIONS.exclude?(position)
+      raise ArgumentError, "Invalid position: #{position}. Valid positions are: #{POSITIONS.join(", ")}"
+    end
+
+    @container_class = [
+      container_class,
+      "dropdown",
+      ("dropdown-" + position),
+      ("dropdown-hover" if hover),
+      ("dropdown-end" if align_end)
+    ].flatten.compact.uniq
+
     @trigger_class = trigger_class
-    @menu_class = menu_class
+    @menu_class = [menu_class, "dropdown-content"].flatten.compact.uniq
     @item_class = item_class
   end
 end
