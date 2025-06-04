de706bf269f872715de53cc175dbd9c44c687326
Mike Repec <mikerepec@gmail.com>
2025-06-04 21:09:33 +0200

    chore: adjust dropdown trigger text in hover preview

--- test/components/previews/dropdown_component_preview.rb
+++ test/components/previews/dropdown_component_preview.rb
@@ -13,7 +13,7 @@ class DropdownComponentPreview < ViewComponent::Preview
 
   def hover
     render(DropdownComponent.new(hover: true)) do |c|
-      c.with_trigger { "Dropdown Hover Trigger" }
+      c.with_trigger { "Dropdown Trigger - Hover" }
 
       c.with_item { tag.a "Item 1", href: "#" }
       c.with_item { tag.a "Item 2", href: "#" }
